ef491ed45a272b6200bc36060eb9bd94531971d3
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2022-11-06 15:13:59 +0900

    WIP (6)

--- FRHED/PreLink.bat
+++ FRHED/PreLink.bat
@@ -2,12 +2,13 @@ cd
 echo %0
 echo $(IntDir) = %1
 echo $(TargetPath) = %2
+echo $(PlatformTarget) = %3
 cd ..\Translations\Frhed
 cscript CreateMasterPotFile.vbs
 cscript UpdatePoFilesFromPotFile.vbs
 cd ..\..\FRHED
 rc "/fo%~1\lang.res" /i.. ..\Translations\Frhed\heksedit.rc
 mkdir "%~2\..\Languages"
-link /DLL /NOENTRY /MACHINE:IX86 "/OUT:%~2\..\Languages\heksedit.lng" "%~1\lang.res"
+link /DLL /NOENTRY /MACHINE:%~3 "/OUT:%~2\..\Languages\heksedit.lng" "%~1\lang.res"
 editbin.exe /OSVERSION:4.0 /SUBSYSTEM:WINDOWS,4.0 "%~2\..\Languages\heksedit.lng"
 copy ..\Translations\Frhed\*.po "%~2\..\Languages"
